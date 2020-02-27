9279821af6ff596155459511d466cc75b529fdf2
szhan <sophia.weiwei.zhang@kingland.com>
2020-02-27 10:37:11 +0800

    change project structure and add todomvc things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+const todos = [
+  // {
+  //   id: 1,
+  //   title: 'eating',
+  //   done: true
+  // },
+  // {
+  //   id: 2,
+  //   title: 'drinking',
+  //   done: true
+  // },
+  // {
+  //   id: 3,
+  //   title: 'laughing',
+  //   done: true
+  // }
+]
+
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  public todos: {
+    id: number;
+    title: string;
+    done: boolean
+  }[] = todos;
+  addTodo(): void {
+    console.log('dsaf');
+  }
 }
